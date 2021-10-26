cbe665ee75865afe0574de071ed79058f705629e
BenjaminXiang <xiangl3@mail.sutech.edu.cn>
2021-10-26 16:15:19 +0800

    Feat: Update CPP implement

--- src/main/cpp/WaveSingalStruct.h
+++ src/main/cpp/WaveSingalStruct.h
@@ -7,4 +7,10 @@ struct WaveSingal {
     uint16_t pos;
 };
 
+struct SampleInfo {
+    int sample_id;
+    std::string sample_name;
+    long long block_length;
+};
+
 #endif //WAVEFORMRETRIEVE_WAVESINGALSTRUCT_H
