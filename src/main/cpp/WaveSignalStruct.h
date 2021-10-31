7e28026a7f245e2e86271b27411e673df1dd3881
hezonghan <11812917@mail.sustech.edu.cn>
2021-10-31 09:02:20 +0800

    fix types CE in WaveSignalStruct.h

--- src/main/cpp/WaveSignalStruct.h
+++ src/main/cpp/WaveSignalStruct.h
@@ -1,10 +1,12 @@
 #ifndef WAVEFORMRETRIEVE_WAVESIGNALSTRUCT_H
 #define WAVEFORMRETRIEVE_WAVESIGNALSTRUCT_H
 
+#include <string>
+
 struct WaveSignal {
     char* signal_name;
-    int8_t fid;
-    uint16_t pos;
+    __int8_t fid;
+    __uint16_t pos;
 };
 
 struct SampleInfo {
