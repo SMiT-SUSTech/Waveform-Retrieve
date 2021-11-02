14f8f3afb5b9bb5646de7ceeefecb0e88c73bfcb
BenjaminXiang <xiangl3@mail.sutech.edu.cn>
2021-11-02 21:56:57 +0800

    Update: Update SQLiteUtils

--- src/main/cpp/SQLiteUtils.cpp
+++ src/main/cpp/SQLiteUtils.cpp
@@ -1,147 +1,268 @@
 #include "SQLiteUtils.h"
 
-sqlite3* SQLiteUtils::open_table() {
-    sqlite3 *db;
-    int rc;
+/** SQLite Accessing Utils. */
 
-    rc = sqlite3_open(table_name, &db);
-    if (rc) {
-        fprintf(stderr, "Cant't open database: %s\n", sqlite3_errmsg(db));
-        exit(0);
-    } else {
-        fprintf(stdout, "Opened database successfully\n");
-    }
-
-    return db;
+bool WavePrj::is_table_exist(const char* k_table_name) {
+    return this->db->tableExists(k_table_name);
 }
 
-bool SQLiteUtils::is_table_exist(const std::string &kTableName) {
-    std::ostringstream sql;
-    sql << "SELECT count(*) FROM sqlite_master WHERE type='table' and NAME='" << kTableName << "';";
-    sqlite3_stmt* stmt0 = NULL;
-    if (sqlite3_prepare_v2(db, sql.str().c_str(), strlen(sql.str().c_str()), &stmt0, NULL) != SQLITE_OK) {
-        if (stmt0) {
-            sqlite3_finalize(stmt0);
+void WavePrj::exec(const char* sql_format_str , ...) {
+    char sql_buf[8192] = {0};
+
+    va_list p;
+    va_start(p, sql_format_str);
+    vsprintf(sql_buf, sql_format_str, p);
+    va_end(p);
+
+    std::cout << "Executing SQL: [" << sql_buf << "]" << std::endl;
+    this->db->execDML(sql_buf);
+}
+
+/** SMiT domain-specific operations. */
+
+const char* SAMPLE_INFO_TABLE_NAME = "SampleInfo";
+const char* WAVE_SIGNAL_TABLE_NAME = "WaveSignal";
+
+void WavePrj::create_sampleinfo_table() {
+    if(is_table_exist(SAMPLE_INFO_TABLE_NAME)) return;
+    exec("CREATE TABLE %s (\n" \
+            "SampleID      INTEGER          PRIMARY KEY AUTOINCREMENT  NOT NULL,\n" \
+            "SampleName    VARCHAR(1024)    UNIQUE                     NOT NULL,\n" \
+            "BlockLength   INT(64)                                     NOT NULL);\n",
+            SAMPLE_INFO_TABLE_NAME);
+}
+
+void WavePrj::insert_sampleinfo(const std::string &sample_name , const long long &length) {
+    try{
+        SampleInfo sample_info_obj = select_sampleinfo(sample_name);
+
+        throw WaveSysException(WaveSysException::SAMPLE_INFO_RECORD_ALREADY_EXISTS , "insert_sampleinfo: Sample name already exists!");
+    } catch (WaveSysException& e) {
+        if(e.code != WaveSysException::NO_SAMPLE_INFO_RECORDS) throw e;
+    }
+
+    exec("INSERT INTO %s (SampleName, BlockLength) VALUES ('%s', %lld);" , SAMPLE_INFO_TABLE_NAME , sample_name.c_str() , length);
+}
+
+SampleInfo WavePrj::select_sampleinfo(const std::string &sample_name) {
+    char sql_buf[8192] = {0};
+    sprintf(sql_buf , "SELECT * FROM %s WHERE SampleName='%s';" , SAMPLE_INFO_TABLE_NAME , sample_name.c_str());
+    std::cout << "Executing SQL: [" << sql_buf << "]" << std::endl;
+
+    smit::sql::sqlite3_query result_1 = this->db->execQuery(sql_buf);
+
+    if(result_1.eof()) {
+        throw WaveSysException(WaveSysException::NO_SAMPLE_INFO_RECORDS , "select_sampleinfo: No records found!");
+    }
+
+    SampleInfo sample_info_obj;
+    sample_info_obj.sample_id    = result_1.getIntField("SampleID");
+    sample_info_obj.sample_name  = result_1.getStringField("SampleName");  // FIXME  returns char*
+    sample_info_obj.block_length = result_1.getInt64Field("BlockLength");  // FIXME  returns __int64.  // block_length max 46 bits.
+
+    result_1.nextRow();
+
+    if(! result_1.eof()) {
+        throw WaveSysException(WaveSysException::SEVERAL_SAMPLE_INFO_RECORDS , "select_sampleinfo: More than one records found!");
+    }
+
+    return sample_info_obj;
+}
+
+void WavePrj::create_wavesignal_table() {
+    if(is_table_exist(WAVE_SIGNAL_TABLE_NAME)) return;
+    exec("CREATE TABLE %s (\n" \
+            "SID         INTEGER         PRIMARY KEY AUTOINCREMENT  NOT NULL,\n" \
+            "SampleID    INT(32)                                    NOT NULL,\n" \
+            "SignalName  VARCHAR(8192)   UNIQUE                     NOT NULL,\n" \
+            "FID         INT(16)                                    NOT NULL,\n" \
+            "Pos         INT(16)                                    NOT NULL);\n",
+            WAVE_SIGNAL_TABLE_NAME);
+
+    exec("CREATE INDEX sample_id_index ON %s (SampleID);" , WAVE_SIGNAL_TABLE_NAME);
+}
+
+void WavePrj::insert_wavesignal(const std::string &sample_name, const WaveSignal &wave_signal) {
+    SampleInfo sample_info_obj = select_sampleinfo(sample_name);
+
+    // TODO  judge whether signal exist ...
+
+    exec("INSERT INTO WaveSignal (SampleID, SignalName, FID, Pos) VALUES (%d , '%s', %d , %d);" , 
+            sample_info_obj.sample_id , wave_signal.signal_name.c_str(), wave_signal.fid , wave_signal.pos);  // FIXME  the second param should be wave_signal.signal_name, rather than sample_name
+}
+
+void WavePrj::batch_insert_wavesignal(const std::string &sample_name, const std::vector<WaveSignal> &signal_list) {
+    SampleInfo sample_info_obj = select_sampleinfo(sample_name);
+        smit::sql::sqlite3_statement compiled = this->db->compileStatement("INSERT INTO WaveSignal (SampleID, SignalName, FID, Pos) VALUES(?, ?, ?, ?);");
+
+        for (const auto &signal: signal_list) {
+            compiled.bind(1 , sample_info_obj.sample_id);
+            compiled.bind(2 , signal.signal_name.c_str());
+            compiled.bind(3 , signal.fid);
+            compiled.bind(4 , signal.pos);
+
+            compiled.execDML();
+            compiled.reset();
         }
-        sqlite3_close(db);
-        fprintf(stderr, "Failed to prepare_v2 the table: %s, error: %s\n", kTableName.c_str(),
-                sqlite3_errmsg(db));
-        exit(0);
-    }
-    int r = sqlite3_step(stmt0);
-    if (r == SQLITE_DONE) {
-        return false;
-    } else if (r == SQLITE_ROW) {
-        return true;
-    } else {
-        fprintf(stderr, "Failed to determine the existence of the table: %s, error: %s\n", kTableName.c_str(),
-                sqlite3_errmsg(db));
-        exit(0);
-    }
+
+        compiled.finalize();
 }
 
-void SQLiteUtils::ddl_exec(const std::string &sql) {
-    int rc;
-    char *zErrMsg = nullptr;
+std::vector<WaveSignal> WavePrj::select_wavesignal(const std::string &sample_name) {
+    SampleInfo sample_info_obj = select_sampleinfo(sample_name);
 
-    rc = sqlite3_exec(db, sql.c_str(), nullptr, 0, &zErrMsg);
+    char sql_buf[8192] = {0};
+    sprintf(sql_buf , "SELECT * FROM %s WHERE SampleID=%d;" , WAVE_SIGNAL_TABLE_NAME , sample_info_obj.sample_id);
+    std::cout << "Executing SQL: [" << sql_buf << "]" << std::endl;
 
-    if( rc != SQLITE_OK ){
-        fprintf(stderr, "SQL error: %s\n", zErrMsg);
-        sqlite3_free(zErrMsg);
-    } else {
-        fprintf(stdout, "Records created successfully\n");
-    }
-}
+    smit::sql::sqlite3_query result_1 = this->db->execQuery(sql_buf);
 
-void SQLiteUtils::create_sampleinfo_table() {
-    if (is_table_exist("SampleInfo")) {
-        return;
-    }
-    const std::string sql = "CREATE TABLE  SampleInfo(" \
-                            "SampleID      INTEGER          PRIMARY KEY AUTOINCREMENT  NOT NULL," \
-                            "SampleName    VARCHAR(1024)                               NOT NULL," \
-                            "BlockLength   INT(64)                                     NOT NULL);";
-    SQLiteUtils::ddl_exec(sql);
-}
+    std::vector<WaveSignal> v;
+    while(! result_1.eof()) {
+        WaveSignal wave_signal_obj;
 
-void SQLiteUtils::create_wavesignal_table() {
-    if (is_table_exist("WaveSignal")) {
-        return;
-    }
-    const std::string sql = "CREATE TABLE WaveSignal(" \
-                            "SID         INTEGER         PRIMARY KEY AUTOINCREMENT  NOT NULL," \
-                            "SampleID    INT(32)         UNIQUE INDEX               NOT NULL," \
-                            "SignalName  VARCHAR(8192)                              NOT NULL," \
-                            "FID         INT(16)                                    NOT NULL," \
-                            "Pos         INT(16)                                    NOT NULL);";
-    SQLiteUtils::ddl_exec(sql);
-}
+        wave_signal_obj.signal_name = result_1.getStringField("SignalName");
 
-void SQLiteUtils::insert_sampleinfo(const std::string &sample_name, const int &length) {
-    std::ostringstream insert_sql;
-    insert_sql << "INSERT INTO SampleInfo (SampleName, BlockLength) " \
-                  "VALUES ('" << sample_name << "', " << length << ");";
-    SQLiteUtils::ddl_exec(insert_sql.str());
-}
+        wave_signal_obj.fid = result_1.getIntField("FID");
+        wave_signal_obj.pos = result_1.getIntField("Pos");
 
-SampleInfo SQLiteUtils::select_sampleinfo(const std::string &sample_name) {
-    int rc = 0;
-    int row_number = 0;
-    int col_number = 0;
-    char **azResult;
-    char *zErrMsg = nullptr;
+        v.push_back(wave_signal_obj);
 
-    std::ostringstream query;
-    query << "SELECT * FROM SampleInfo WHERE SampleName='" << sample_name << "';";
-    rc = sqlite3_get_table(db, query.str().c_str(), &azResult, &row_number, &col_number, &zErrMsg);
-    SampleInfo result;
-    if (rc == SQLITE_OK) {
-        int index = col_number;
-        for (int i=0; i<row_number; ++i) {
-            for (int j=0; j<col_number; ++j) {
-                if (strcmp(azResult[i], "SampleID") == 0) {
-                    result.sample_id = atoi(azResult[index++]);
-                } else if (strcmp(azResult[i], "SampleName") == 0) {
-                    result.sample_name = azResult[index++];
-                } else if (strcmp(azResult[i], "BlockLength") == 0) {
-                    result.block_length = atoi(azResult[index++]);
-                }
-            }
-        }
-    }
-    return result;
-}
-
-void SQLiteUtils::insert_wavesignal(const std::string &sample_name, const WaveSignal &wave_signal) {
-    SampleInfo sample_info = SQLiteUtils::select_sampleinfo(sample_name) ;
-    std::ostringstream insert_sql;
-    insert_sql << "INSERT INTO WaveSignal (SampleID, SignalName, FID, Pos) " \
-                  "VALUES (" << sample_info.sample_id << ", '" << sample_name << "', " << wave_signal.fid << \
-                  ", " << wave_signal.pos << ");";
-    SQLiteUtils::ddl_exec(insert_sql.str());
-}
-
-void SQLiteUtils::batch_insert_wavesignal(const std::string &sample_name, const std::vector<WaveSignal> &signal_list) {
-    SampleInfo sample_info = SQLiteUtils::select_sampleinfo(sample_name);
-    const char *insert_sql = "INSERT INTO WaveSignal (SampleID, SignalName, FID, Pos) VALUES(?, ?, ?, ?);";
-    sqlite3_stmt* stmt = nullptr;
-    const char *pzTail;
-
-    sqlite3_prepare_v2(db, insert_sql, strlen(insert_sql), &stmt, &pzTail);
-
-    int col = 1;
-    for (const auto &signal: signal_list) {
-        sqlite3_bind_int(stmt, col++, sample_info.sample_id);
-        sqlite3_bind_text(stmt, col++, signal.signal_name, strlen(signal.signal_name), nullptr);
-        sqlite3_bind_int(stmt, col++, signal.fid);
-        sqlite3_bind_int(stmt, col++, signal.pos);
-
-        sqlite3_step(stmt);
-        sqlite3_reset(stmt);
+        result_1.nextRow();
     }
 
-    sqlite3_finalize(stmt);
+    return v;
 }
 
-void SQLiteUtils::get_signal_hbase_info(const char *signal_name) {}
+
+/** SMiT required APIs */
+
+#define SUCCESS_CODE 0
+#define FAIL_CODE 1
+
+WavePrj*  WaveUtils::current_project = nullptr;
+std::string  WaveUtils::current_sample = "";  // FIXME  assign NULL to a non-pointer ?? Now change to pointer definition.
+
+int32_t WaveUtils::SetPrj(std::string PrjName) {
+    current_sample = "";
+
+    try {
+        if(current_project != nullptr) current_project = nullptr;  // TODO  check: should invoke ~WavePrj()
+
+        current_project = new WavePrj((PrjName + ".db").c_str());
+        current_project->create_sampleinfo_table();
+        current_project->create_wavesignal_table();
+
+    } catch (smit::sql::sqlite3_exception& e) {
+        std::cout << e.errorMessage() << std::endl;
+        current_project = nullptr;
+        return FAIL_CODE;
+    }
+    
+    std::cout << "\nSwitched to project [" << PrjName << "]\n" << std::endl;
+    return SUCCESS_CODE;
+}
+
+int32_t WaveUtils::SetSample(std::string SampleName , long long &block_length) {
+    if(current_project == nullptr) {
+        std::cout << "Project not set." << std::endl;
+        return FAIL_CODE;
+    }
+
+    try {
+        current_sample = SampleName;
+        current_project->insert_sampleinfo(SampleName , block_length);
+
+    } catch (smit::sql::sqlite3_exception& e) {
+        std::cout << e.errorMessage() << std::endl;
+        current_sample = "";
+        return FAIL_CODE;
+    } catch (WaveSysException& e) {
+        // Catch the exception and do nothing here, because it's ok to have existing sample name.
+    }
+
+    std::cout << "\nSwitched to sample [" << SampleName << "]\n" << std::endl;
+    return SUCCESS_CODE;
+}
+
+int32_t WaveUtils::StoreWaveSignal(std::vector<WaveSignal> &SignalList) {
+    if(current_project == nullptr) {
+        std::cout << "Project not set." << std::endl;
+        return FAIL_CODE;
+    }
+    if(current_sample == "") {
+        std::cout << "Sample name not set." << std::endl;
+        return FAIL_CODE;
+    }
+
+    try {
+        current_project->batch_insert_wavesignal(current_sample , SignalList);
+
+    } catch (smit::sql::sqlite3_exception& e) {
+        std::cout << e.errorMessage() << std::endl;
+        return FAIL_CODE;
+
+    } catch (WaveSysException& e) {
+        std::cout << e.errorMessage() << std::endl;
+        return FAIL_CODE;
+    }
+
+    return SUCCESS_CODE;
+}
+
+inline bool check_file_exists(const char* path) {  // TODO  check
+    FILE* fp = fopen(path , "r");
+    if(fp == nullptr) return false;
+    fclose(fp);
+    return true;
+}
+
+int32_t WaveUtils::CheckSampleExists(std::string PrjName , std::string SampleName) {
+    const char* db_name = (PrjName + ".db").c_str();
+    if(! check_file_exists(db_name)) {
+        std::cout << "Project [" << PrjName << "] not found!" << std::endl;
+        return FAIL_CODE;
+    }
+
+    try {
+        WavePrj* prj2 = new WavePrj(db_name);
+        prj2->select_sampleinfo(SampleName);  // if not found or found multiple records, it will throw WaveSysException.
+        prj2 = nullptr;
+
+    } catch (smit::sql::sqlite3_exception& e) {
+        std::cout << e.errorMessage() << std::endl;
+        return FAIL_CODE;
+
+    } catch (WaveSysException& e) {
+        std::cout << e.errorMessage() << std::endl;
+        return FAIL_CODE;
+    }
+
+    return SUCCESS_CODE;
+}
+
+int32_t WaveUtils::GetWaveSignalList(std::vector<WaveSignal> &SignalList) {
+    if(current_project == nullptr) {
+        std::cout << "Project not set." << std::endl;
+        return FAIL_CODE;
+    }
+    if(current_sample == "") {
+        std::cout << "Sample name not set." << std::endl;
+        return FAIL_CODE;
+    }
+
+    try {
+        SignalList = current_project->select_wavesignal(current_sample);
+
+    } catch (smit::sql::sqlite3_exception& e) {
+        std::cout << e.errorMessage() << std::endl;
+        return FAIL_CODE;
+
+    } catch (WaveSysException& e) {
+        std::cout << e.errorMessage() << std::endl;
+        return FAIL_CODE;
+    }
+
+    return SUCCESS_CODE;
+}
+
--- src/main/cpp/SQLiteUtils.h
+++ src/main/cpp/SQLiteUtils.h
@@ -1,54 +1,100 @@
 #ifndef CPP_SQLITEUTILS_H
 #define CPP_SQLITEUTILS_H
 
-#include <sqlite3.h>
-#include <cstdlib>
-#include <cstdio>
-#include <cstring>
-#include <string>
-#include <vector>
+#include "sqlite_src/CppSQLite3.h"
+#include "../cpp/WaveSignalStruct.h"
+
 #include <algorithm>
-#include <sstream>
-#include "WaveSignalStruct.h"
+#include <iostream>
+#include <vector>
 
-class SQLiteUtils {
-    char* table_name = nullptr;
-    sqlite3 *db = nullptr;
-    void ddl_exec(const std::string &sql);
-public:
-    SQLiteUtils() = default;
-    SQLiteUtils(const char* name) {
-        const size_t name_length = strlen(name);
-        table_name = new char[name_length + 1]{};
-        std::copy_n(name, name_length, table_name);
-        int rc;
 
-        rc = sqlite3_open(table_name, &db);
-        if (rc) {
-            fprintf(stderr, "Cant't open database: %s\n", sqlite3_errmsg(db));
-            exit(0);
-        } else {
-            fprintf(stdout, "Opened database successfully\n");
+class WaveSysException {
+
+    public:
+        int code;
+        const char* msg;
+
+        WaveSysException(int _code , const char* _msg) {
+            this->code = _code;
+            this->msg = _msg;
         }
-    }
-    ~SQLiteUtils() {
-        table_name = nullptr;
-        if (db != nullptr) {
-            sqlite3_close(db);
-        }
-    }
 
-    sqlite3* open_table();
-//    void query_exec(const std::string &sql)qli
-    bool is_table_exist(const std::string &kTableName);
-    void create_sampleinfo_table();
-    void create_wavesignal_table();
-    void insert_sampleinfo(const std::string &sample_name, const int &length);
-    SampleInfo select_sampleinfo(const std::string &sample_name);
-    void insert_wavesignal(const std::string &sample_name, const WaveSignal &wave_signal);
-    void batch_insert_wavesignal(const std::string &sample_name, const std::vector<WaveSignal> &signal_list);
-//    WaveSignal select_wavesignal(const std::string );
-    void get_signal_hbase_info(const char* signal_name);
+        ~WaveSysException() {
+            this->msg = nullptr;  // TODO
+        }
+
+        static const int NO_SAMPLE_INFO_RECORDS = 100;
+        static const int SEVERAL_SAMPLE_INFO_RECORDS = 101;
+        static const int SAMPLE_INFO_RECORD_ALREADY_EXISTS = 102;
+
+        const char* errorMessage() {
+            return this->msg;
+        }
 };
 
-#endif //CPP_SQLITEUTILS_H
+
+class WavePrj {
+    private:
+        char* db_name = nullptr;  // Each wave project corresponds to a SQLite ".db" file, and the filename will be the same as the project name.
+        smit::sql::sqlite3_db* db;
+
+    public:
+        WavePrj(const char* name) {
+            const size_t name_length = strlen(name);
+            this->db_name = new char[name_length + 1]{};
+            std::copy_n(name , name_length , this->db_name);
+            
+            this->db = new smit::sql::sqlite3_db();
+            if(this->db == nullptr) {
+                std::cout << "Create m_pSqlDb failed!!!" << std::endl;
+                exit(1);
+            }
+
+            this->db->open(name);
+        }
+
+        ~WavePrj() {
+            std::cout << "Closing project [" << this->db_name << "]." << std::endl;
+            delete this->db_name;
+            this->db_name = nullptr;
+            if(this->db != nullptr) this->db->close();
+            delete this->db;
+        }
+
+        /** SQLite Accessing Utils. */
+        bool is_table_exist(const char* kTableName);
+        void exec(const char* sql_format_str , ...);
+
+        /** SMiT domain-specific operations. */
+        void create_sampleinfo_table();
+        void insert_sampleinfo(const std::string &kSampleName , const long long &kLength);
+        SampleInfo select_sampleinfo(const std::string &sample_name);
+
+        void create_wavesignal_table();
+        void insert_wavesignal(const std::string &sample_name, const WaveSignal &wave_signal);
+        void batch_insert_wavesignal(const std::string &sample_name, const std::vector<WaveSignal> &signal_list);
+        std::vector<WaveSignal> select_wavesignal(const std::string &sample_name);
+};
+
+class WaveUtils {
+
+    private:
+        static WavePrj* current_project;
+        static std::string current_sample;
+
+    public:
+        /** SMiT required APIs */
+        static void getSampleName() {
+            std::cout << "&&&&&&main sample name: " << current_sample << std::endl;
+        }
+        static int32_t SetPrj(std::string PrjName);  // SetPrjDB
+        static int32_t SetSample(std::string SampleName , long long &block_length);  // SetWaveDB  // WaveName
+        static int32_t StoreWaveSignal(std::vector<WaveSignal> &SignalList);
+
+        static int32_t CheckSampleExists(std::string PrjName , std::string SampleName);  // GetWaveDB
+        static int32_t GetWaveSignalList(std::vector<WaveSignal> &SignalList);
+
+};
+
+#endif //CPP_SQLITEUTILS_HZH_H
